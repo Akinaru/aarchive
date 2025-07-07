153eeac6f55685a6daac264214a22245ee6032fe
Akinaru <maxime.utchimata@gmail.com>
2025-07-07 16:21:49 +0200

    env example et seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,17 @@
 const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
+require('dotenv').config(); // pour charger le .env
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword = await bcrypt.hash("MOT DE PASSE ICI", 10);
+  const password = process.env.USER_PASS;
+
+  if (!password) {
+    throw new Error("USER_PASS manquant dans le fichier .env");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = await prisma.utilisateur.create({
     data: {
@@ -18,7 +25,11 @@ async function main() {
 }
 
 main()
-  .catch((e) => console.error(e))
+  .catch((e) => {
+    console.error("Erreur lors du seed :", e);
+    process.exit(1);
+  })
   .finally(() => prisma.$disconnect());
 
 // node .\prisma\seed.ts
+// npx ts-node prisma/seed.ts
